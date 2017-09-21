a833f454a04881886519947ab1210cc960764662
az9000 <fawzi.alami@yahoo.com>
2017-09-21 17:04:01 -0500

    Adding page 147 code

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-const question = "What is Superman\'s real name?";
-const answer = prompt(question);
-alert(`You answered ${answer}`);
+const quiz = [['What is Superman\s real name?', 'Clark Kent'],['What is Batman\s real name?', 'Bruce Wayne'],['What is Wonder Woman\s real name?', 'Diana Prince']];
+let score = 0;  // initialize score
+for (const [question,answer] of quiz) {
+    const response = prompt(question);
+    if (response === answer) {
+        alert('Correct!');
+        score ++;
+    }
+    else {
+        alert(`Wrong! The correct answer was ${answer}`);
+    }
+}
+// At the end of the game, report the player's score
+alert(`Game Over! Your score is ${score}`);
